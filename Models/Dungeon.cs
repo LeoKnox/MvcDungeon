34ecd53946c46480d7527922e01fd69cd2ecea7c
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-11 16:17:05 -0700

    Add files via upload

---
+++ Models/Dungeon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcDungeon.Models
+{
+    public class Dungeon
+    {
+        public int Id { get; set; }
+        public string RoomName { get; set; }
+        public string Type { get; set; }
+        public int length { get; set; }
+        public int width { get; set; }
+    }
+}
